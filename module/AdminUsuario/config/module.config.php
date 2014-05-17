beb0f0ee73c113b17dae503e51a647ea9360bf15
Walter Nascimento <walter@m2smart.com.br>
2014-05-17 16:09:45 -0400

    listando usuario
    Signed-off-by: Walter Nascimento <walter@m2smart.com.br>

--- module/AdminUsuario/config/module.config.php
+++ module/AdminUsuario/config/module.config.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AdminProduto;
+namespace AdminUsuario;
 
 //confira rotas
 return array(
--- module/AdminUsuario/src/AdminUsuario/Controller/AdminUsuarioController.php
+++ module/AdminUsuario/src/AdminUsuario/Controller/AdminUsuarioController.php
@@ -4,8 +4,7 @@
 
 namespace AdminUsuario\Controller;
 
-use Admin\Controller\CrudController,
-    Zend\View\Model\ViewModel;
+use Admin\Controller\CrudController;
 
 class AdminUsuarioController extends CrudController {
 
@@ -15,12 +14,6 @@ class AdminUsuarioController extends CrudController {
         $this->form = "AdminUsuario\Form\Usuario";
         $this->controller = "adminusuario";
         $this->route = "usuario";
-    }
-
-    public function indexAction() {
-
-        $list = "sdçfl";
-        return new ViewModel(array('data' => $list));
     }
 
 }
